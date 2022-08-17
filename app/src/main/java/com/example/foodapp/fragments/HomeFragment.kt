a5d8124c8e69172e3717f7cee3ece230d43c9de0
pkadziela <piotr.kadziela@lppsa.com>
2022-08-17 16:28:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/foodapp/fragments/HomeFragment.kt
@@ -0,0 +1,50 @@
+package com.example.foodapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.example.foodapp.databinding.FragmentHomeBinding
+import com.example.foodapp.pojo.Meal
+import com.example.foodapp.viewModel.HomeViewModel
+
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var homeMvvm: HomeViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        homeMvvm = ViewModelProvider(this)[HomeViewModel::class.java]
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        homeMvvm.getRandomMeal()
+        observeRandomMeal()
+    }
+
+    private fun observeRandomMeal() {
+        homeMvvm.observeRandomMealLiveData().observe(viewLifecycleOwner, object : Observer<Meal>{
+            override fun onChanged(t: Meal?) {
+                Glide.with(this@HomeFragment)
+                    .load(t!!.strMealThumb)
+                    .into(binding.imgRandomMeal)
+            }
+        })
+    }
+}
