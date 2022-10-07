12bbf5c485785e6a0e1696ab669b283b23e03313
pkadziela <piotr.kadziela@lppsa.com>
2022-10-07 15:02:16 +0200

    Add Room

---
+++ app/src/main/java/com/example/foodapp/db/MealDao.kt
@@ -0,0 +1,17 @@
+package com.example.foodapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.foodapp.pojo.Meal
+
+@Dao
+interface MealDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun update(meal: Meal)
+
+    @Delete
+    suspend fun delete(meal: Meal)
+
+    @Query("SELECT * FROM mealInformation")
+    fun getAll(): LiveData<List<Meal>>
+}
